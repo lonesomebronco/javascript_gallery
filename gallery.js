09e5fb09a4ca4c43f183128ea2068d1ea4091142
Vaibhav Sharma <34615543+lonesomebronco@users.noreply.github.com>
2020-05-03 00:21:57 +0530

    Add files via upload

---
+++ gallery.js
@@ -0,0 +1,10 @@
+function upDate(previewPic){
+  var imageDiv = document.getElementById("image");
+  imageDiv.style.backgroundImage = "url(" + previewPic.src + ")";
+   imageDiv.innerHTML = previewPic.alt;
+}
+function unDo(){
+    var imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "url('')";
+    imageDiv.innerHTML = "Hover over an image below to display here.";
+}
